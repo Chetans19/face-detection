31e5dabb2e13b69c9eab0ea1631cfef52d0b04ef
champ <itschetanshinde@gmail.com>
2020-07-10 17:11:08 +0530

    initial commit

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Tilt from 'react-tilt';
+import './Logo.css'
+import icon from './icon.png'
+
+const Logo = () => {
+    return (
+        <div className='ma4 mt0'>
+            <Tilt className="Tilt br2 shadow-2" options={{ max : 25 }} 
+            style={{ height: 150, width: 150 }} >
+                
+            <div className="Tilt-inner" style={{paddingTop: '40px'}}> 
+            <img alt='logo' src={icon} />
+           </div> 
+            
+            
+         </Tilt>
+
+         </div>
+    )
+}
+
+export default Logo;
